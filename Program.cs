4d2badadd9f34a0709c7342b3160389eb031e94d
TAH4uk <pcs_zero@inbox.ru>
2022-08-28 18:50:21 +0300

    zadachi

--- Program.cs
+++ Program.cs
@@ -1,28 +1,28 @@
 ﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// Console.Write("Введите пятизначное число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите положительное пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
 
-// while (number < 9999 || number > 99999)
-// {
-//     Console.Write("Вы ввели неправильное число! Введите пятизначное число: ");
-//     number = int.Parse(Console.ReadLine());
-// }
+while (number < 9999 || number > 99999)
+{
+    Console.Write("Вы ввели неправильное число! Введите положительное пятизначное число: ");
+    number = int.Parse(Console.ReadLine());
+}
 
-// string result = number.ToString();
-// int result0 = result[0];
-// int result1 = result[1];
-// int result3 = result[3];
-// int result4 = result[4];
+string result = number.ToString();
+int result0 = result[0];
+int result1 = result[1];
+int result3 = result[3];
+int result4 = result[4];
 
-// if (result0 == result4 & result1 == result3)
-// {
-//     Console.WriteLine($"Число {number} является полиндромом");
-// }
-// else
-// {
-//     Console.WriteLine($"Число {number} НЕ является полиндромом");
-// }
+if (result0 == result4 & result1 == result3)
+{
+    Console.WriteLine($"Число {number} является полиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ является полиндромом");
+}
 
 // Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
@@ -43,18 +43,18 @@
 
 // Console.WriteLine($"Расстояние между двумя точками с координатами ({x1}, {y1}, {z1}) и ({x2}, {y2}, {z2}) равно {result}");
 
-// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-Console.Write("Введите положительное число N: ");
-int N = int.Parse(Console.ReadLine());
+// Console.Write("Введите положительное число N: ");
+// int N = int.Parse(Console.ReadLine());
 
-while (N < 0)
-{
-    Console.Write("Вы ввели неправильное число! Введите положительное число N: ");
-    N = int.Parse(Console.ReadLine());
-}
+// while (N < 0)
+// {
+//     Console.Write("Вы ввели неправильное число! Введите положительное число N: ");
+//     N = int.Parse(Console.ReadLine());
+// }
 
-for (int i = 1; i <= N; i++)
-{
-    Console.Write($"{i * i * i}, ");
-}
+// for (int i = 1; i <= N; i++)
+// {
+//     Console.Write($"{i * i * i}, ");
+// }
